bdf4d4154ee40630ac0177a36a5d135013707a24
sho6125 <sho.6125@icloud.com>
2018-11-27 19:43:03 +0900

    commit4

--- src/Main3.java
+++ src/Main3.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main3 {
     public static void main(String[] args) {
         BookShelf bookShelf = new BookShelf(4);
         bookShelf.appendBook(new Book("Around the World in 80 Days"));
